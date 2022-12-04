d59bce42777420fff6c861e83339cdbd2b0293a3
Prateeksha Ranjan <prateeksha.67@gmail.com>
2022-12-04 22:35:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const twilio = require('twilio');
 const port = process.env.PORT || 3000;
 
-const accountSid = 'ACa50ed6342f695fa378936ad4d13afc51;
+const accountSid = 'ACa50ed6342f695fa378936ad4d13afc51';
 const authToken = '141b3642ac3768112c032e53a5bc3809';
 const client = new twilio(accountSid, authToken);
 
